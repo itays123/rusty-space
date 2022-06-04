92618bbbe1f3b6495e047318095ca68580a023ea
itays123 <itays2005@gmail.com>
2022-06-05 01:35:52 +0300

    FEATURE: Uniting and parallel lines

--- src/line/relations.rs
+++ src/line/relations.rs
@@ -2,6 +2,7 @@
 use crate::vector::Vector;
 use crate::line::Line;
 
+#[derive(Debug, PartialEq)]
 pub enum LineRelations {
     /// The two lines share the exact same points
     Unite,
@@ -14,16 +15,35 @@ pub enum LineRelations {
 }
 
 impl LineRelations {
-    pub fn of(line1: Line, line2: Line) -> LineRelations {
+    pub fn of(line1: &Line, line2: &Line) -> LineRelations {
         if line1.direction.is_lindep(line2.direction) {
             // lines either unite or parallel
-            if line1.is_on_line(line2.point) { Self::Unite }
-            else { Self::Parallel(0.0) }
+            let distance = line1.distance_from_point(line2.point);
+            if distance == 0.0 { Self::Unite } else { Self::Parallel(distance) }
         }
         else {
             let angle = Vector::angle_between(line1.direction, line2.direction);
             // find a point that is on both lines
             Self::Intersect(0.0, angle)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn same_line() {
+        let line1 = Line::new(Vector(0.0, 0.0, 0.0), Vector(1.0, 0.0, 0.0));
+        let line2 = Line::new(Vector(0.0, 0.0, 0.0), Vector(2.0, 0.0, 0.0));
+        assert_eq!(LineRelations::of(&line1, &line2), LineRelations::Unite)
+    }
+
+    #[test]
+    fn parallel_lines() {
+        let line1 = Line::new(Vector(0.0, 0.0, 0.0), Vector(1.0, 0.0, 0.0));
+        let line2 = Line::new(Vector(0.0, 1.0, 0.0), Vector(1.0, 0.0, 0.0));
+        assert_eq!(LineRelations::of(&line1, &line2), LineRelations::Parallel(1.0))
     }
 }
