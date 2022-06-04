cc991c06690e4a998d3e069fa0be1deb2f7ae612
itays123 <itays2005@gmail.com>
2022-06-05 01:27:43 +0300

    FEATURE: Distance between point and line

--- src/line.rs
+++ src/line.rs
@@ -10,27 +10,58 @@ pub struct Line {
 }
 
 impl Line {
+    
     pub fn new(point: Vector, direction: Vector) -> Line {
         Line { point, direction }
     }
 
-    pub fn is_on_line(self, other_point: Vector) -> bool {
-        let Vector(x, y, z) = other_point;
-        // compute t where (x, y, z) = point + t * direction
-        // meaning, x = xp + t * u1 => u1 * t = x - xp
-        let Vector(xp, yp, zp) = self.point;
-        let Vector(u1, u2, u3) = self.direction;
+    pub fn is_on_line(&self, other_point: Vector) -> bool {
+        self.distance_from_point(other_point) == 0.0
+    }
 
-        let sol_x = EquationSolution::compute(u1, x - xp);
-        let sol_y = EquationSolution::compute(u2, y - yp);
-        let sol_z = EquationSolution::compute(u3, z - zp);
+    fn distance_from_point(&self, other_point: Vector) -> f64 {
+        // Find a point p1 = p + tu where op1 * direction = 0
+        // for every dimension of p1:
+        // xp1 = xp + tu1
+        // xp1 - xo = xp - xo + tu1
+        // therefore op1 = (xp - xo + tu1, yp - yo + tu2, zp - zo + tu3)
+        // and op1 * direction = u1(xp - xo + tu1) + u2(yp - yo + tu2) + u3(zp - zo + tu3) = 0
+        // simplify: (u1^2 + u2^2 + u3^2)*t + u1(xp - xo) + u2(yp-yo) + u3(zp - zo) = 0
+        // (u^2) * t + u * (p - o) = 0
+        let coefficient = self.direction * self.direction;
+        let sum = self.direction * (self.point - other_point);
 
-        sol_x == sol_y && sol_x == sol_z && sol_y == sol_z
+        let solution = EquationSolution::compute(coefficient, sum);
+        if let EquationSolution::Real(t) = solution {
+            let differece = self.point - other_point + t * self.direction;
+            differece.length()
+        }
+        else {
+            // mathematically not possible
+            panic!("This should never happen")
+        }
+
     }
 }
 
 impl PartialEq for Line {
     fn eq(&self, other: &Self) -> bool {
         self.point == other.point && self.direction == other.direction
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn distance_works() {
+        let line = Line::new(Vector(0.0, 0.0, 0.0), Vector(1.0, 0.0, 0.0)); // the x axis
+        let distance = line.distance_from_point(Vector(0.0, 1.0, 0.0));
+        assert_eq!(distance, 1.0);
+        let distance = line.distance_from_point(Vector(0.0, 0.0, 1.0));
+        assert_eq!(distance, 1.0);
+        let distance = line.distance_from_point(Vector(0.0, 0.0, 0.0));
+        assert_eq!(distance, 0.0);
     }
 }
